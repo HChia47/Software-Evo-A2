8775091d6a5d8c7e61db8af1305e1cd349b5a20e
HChia47 <34344440+HChia47@users.noreply.github.com>
2021-03-14 00:22:56 +0100

    working lineCounter.py

--- scripts/lineCounter.py
+++ scripts/lineCounter.py
@@ -1,13 +1,38 @@
 import os
 import csv
-import pandas
+import pandas as pd
+import subprocess
 
 def countLineOfRelease(directory):
-    cmd = 'cloc --quiet --csv {}/src'.format(directory)
-    print(cmd)
-    os.popen(cmd)
+    output = subprocess.check_output(["cloc", "--quiet", "--csv", "{}/src".format(directory)])
+    print(output)
+    splitOutput = output.splitlines()
+    lastLine = splitOutput[-1]
+    lastLineDec = lastLine.decode('utf-8')
+    print(lastLineDec)
+    splitLastLine = lastLineDec.split(",")
+    print(splitLastLine)
+    splitLastLine.remove("SUM")
+    print(splitLastLine)
+    splitLastLine.insert(0, directory)
+    print(splitLastLine)
+    print(type(splitLastLine[0]))
+    return splitLastLine
+    #cmd = 'cloc --quiet --csv {}/src'.format(directory)
+    #print(cmd)
+    #os.popen(cmd)
 
 with os.scandir("/usr/jquery-data") as dirs:
+    listData = []
     for curdir in dirs:
-        print(curdir.name)
-        countLineOfRelease(curdir.name)
+        if curdir.is_dir():
+            print(curdir.name)
+            data = countLineOfRelease(curdir.name)
+            listData.append(data)
+    print(listData)
+    listData.sort(key=lambda s: list(map(int, s[0].split('.'))))
+    print(listData)
+    dataFrame = pd.DataFrame(listData, columns = ["version", "files", "blank", "comment", "code"])
+    dataFrame.to_csv("/out/lineData.csv", index = False)
+
+
